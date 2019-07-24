d8d138e5e57a3452acd46713c8ed70d145c5b82e
unknown <quentin.miolane@epitech.eu>
2019-07-24 10:43:00 +0200

    caroussel + scroll

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Carousel from '../Components/Carousel'
+import ReactPageScroller from "react-page-scroller";
+import '../Styles/Landing.css'
+import Carousel from './components/Carousel'
 
 export default class Home extends React.Component {
 
@@ -8,7 +10,17 @@ export default class Home extends React.Component {
 	}
 
 	render(){
-		return <Carousel></Carousel>
+		return (
+			
+				<div>
+					<ReactPageScroller>
+					<Carousel></Carousel>
+				<div>
+					<h1 className="WelcomeText">Bienvenue sur Dosimex</h1>
+					<p className="Fiftywords">Antoine est la reine des putes mais pour le moment il n'y a que peu de mots alors nous écrivons <br/>encore et encore en espérant atteindre les 50 mots, la question est: Est-ce que l'indentation sera cool or not</p>
+				</div>
+				</ReactPageScroller>
+				</div>
+		);
 	}
-
 }
--- src/Views/components/Carousel.js
+++ src/Views/components/Carousel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from 'react-animated-slider';
-import Car1 from '../Images/image1Carousel.jpg'
+import Car1 from '../../Images/image1Carousel.jpg'
 import 'react-animated-slider/build/horizontal.css';
 
 export default class Carousel extends React.Component {
@@ -11,9 +11,9 @@ export default class Carousel extends React.Component {
 	render(){
 		return(
 		<Slider>
-			<img src={Car1} />
-			<img src="https://s18.postimg.cc/vunvhvvrt/img2.jpg" />
-			<img src="https://s18.postimg.cc/tdc4amjl5/img3.jpg" />
+			<img style={} src={Car1} />
+			<img style={} src="https://s18.postimg.cc/vunvhvvrt/img2.jpg" />
+			<img style={} src="https://s18.postimg.cc/tdc4amjl5/img3.jpg" />
 		</Slider>
 		);
 	}
