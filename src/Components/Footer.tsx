bd19a387a0501dfd646a9a2a94dd8ff87b4cf8f1
mariefj <marie.forja@gmail.com>
2021-06-03 01:10:28 +0200

    feat(): Footer basic

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as CSS from 'csstype';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface IProps {
 }
@@ -24,33 +25,43 @@ class ContactForm extends React.Component<IProps, IState> {
 
 	render() {
 		return (
-			<div>
-				<div>
-					<Image />
-					<p></p>
-					<p></p>
-					<div>
-						<Image />
-						<Image />
-						<p></p>
+			<div style={{display: "flex", flexWrap: "wrap", justifyContent: "center", padding: "30px", backgroundColor: "black", color: "white"}}>
+				<div style={{width: "20%"}}>
+					{/*<Image />*/}
+					<p>Copyright © 2021 Designed by BHC-IT</p>
+					<p>Tous droits réservés </p>
+					<div style={{display: "flex", alignItems: "center"}}>
+						<i className="flaticon-telephone" style={{fontSize: "1.5rem"}}/>
+						<p>06 89 70 90 35</p>
 					</div>
-					<div>
-						<Image />
-						<Image />
-						<p></p>
+					<div style={{display: "flex", alignItems: "center"}}>
+						<div style={{display: "flex", alignItems: "center"}}>
+							<Link href="" replace>
+								<i className="flaticon-youtube" style={{fontSize: "2rem", height: "100%"}}/>
+							</Link>
+							<Link href="" replace>
+								<i className="flaticon-linkedin"style={{fontSize: "1.3rem", height: "100%"}}/>
+							</Link>
+						</div>
+						<div>
+							<p style={{margin: "0", color: "red"}}>Retrouvez-nous</p>
+							<p style={{margin: "0", color: "red"}}>sur nos réseaux</p>
+						</div>
 					</div>
 				</div>
-				<div>
-					<h5></h5>
-					<p></p>
-					<p></p>
-					<p></p>
-				</div>
-				<div>
-					<h5></h5>
-					<p></p>
-					<p></p>
-					<p></p>
+				<div style={{width: "40%", display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+					<div style={{width: "30%"}}>
+						<h5>Ressources</h5>
+						<Link href="/Videos" replace><p>Archives vidéos</p></Link>
+						<Link href="/Manuals" replace><p>Manuels et validations</p></Link>
+						<Link href="/Books" replace><p>Lectures</p></Link>
+					</div>
+					<div style={{width: "30%"}}>
+						<h5>Entreprise</h5>
+						<Link href="/About" replace><p>Qui sommes-nous ?</p></Link>
+						<Link href="/" replace><p>Mentions légales</p></Link>
+						<Link href="/Contact" replace><p>Contact</p></Link>
+					</div>
 				</div>
 			</div>
 		);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../styles/font/flaticon.css'
 import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <>
 		<Navbar />
 		<Component {...pageProps} />
+		<Footer />
 	</>
 
 }
